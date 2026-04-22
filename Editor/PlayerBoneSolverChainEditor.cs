5dbbd5a98fdcb814ce4bd23be6398a4fcfb60818
Nttlong <zugeliang2000@gmail.com>
2026-04-22 11:24:59 +0700

    Review and clear all uneccary files

--- Editor/PlayerBoneSolverChainEditor.cs
+++ Editor/PlayerBoneSolverChainEditor.cs
@@ -1,26 +1,26 @@
-using UnityEngine;
-using UnityEditor;
-using unvs.players; // Phải có namespace này
+//using UnityEngine;
+//using UnityEditor;
+//using unvs.players; // Phải có namespace này
 
-[CustomEditor(typeof(PlayerBoneBase), true)] // 'true' để áp dụng cho cả các class con
-public class PlayerBoneBaseEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+//[CustomEditor(typeof(PlayerBoneBase), true)] // 'true' để áp dụng cho cả các class con
+//public class PlayerBoneBaseEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        DrawDefaultInspector();
 
-        // Ép kiểu về class CHA (Base)
-        PlayerBoneBase script = (PlayerBoneBase)target;
+//        // Ép kiểu về class CHA (Base)
+//        PlayerBoneBase script = (PlayerBoneBase)target;
 
-        GUILayout.Space(10);
+//        GUILayout.Space(10);
 
-        if (GUILayout.Button("Generate Solve Chain", GUILayout.Height(30)))
-        {
-            script.GenerateSolve();
-        }
-        if (GUILayout.Button("Clear Solve Chain", GUILayout.Height(30)))
-        {
-            script.ClearSolve();
-        }
-    }
-}
+//        if (GUILayout.Button("Generate Solve Chain", GUILayout.Height(30)))
+//        {
+//            script.GenerateSolve();
+//        }
+//        if (GUILayout.Button("Clear Solve Chain", GUILayout.Height(30)))
+//        {
+//            script.ClearSolve();
+//        }
+//    }
+//}
--- Runtime/playerobjects/PlayerInteractObjectTeleport.cs
+++
@@ -1,9 +0,0 @@
-//using UnityEngine;
-
-//namespace unvs.playerobjects
-//{
-//    public  class PlayerInteractObjectTeleport : PlayerInteractObject
-//    {
-
-//    }
-//}
