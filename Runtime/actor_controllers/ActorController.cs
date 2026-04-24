d4496274a1b2e01f187b13c6ddc16a6f1d98abca
Nttlong <zugeliang2000@gmail.com>
2026-04-24 20:31:21 +0700

    feat: introduce ActorController with editor utility for basic player component initialization

--- Runtime/actor_controllers/ActorController.cs
+++ Runtime/actor_controllers/ActorController.cs
@@ -1,14 +1,45 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using unvs.actor.player;
 using unvs.game2d.actors;
+using unvs.game2d.objects.editor;
 
 namespace unvs.controllers
 {
     [Serializable]
-    public class ActorController : unvs.types.UnvsEditableProperty
+    public partial class ActorController : unvs.types.UnvsEditableProperty
     {
         public MonoBehaviour owner;
         public UnvsPlayer Player;
     }
+#if UNITY_EDITOR
+    public partial class ActorController : unvs.types.UnvsEditableProperty
+    {
+        [UnvsButton("Create default controller")]
+        public void EditorCreateDefaltPlayerController()
+        {
+            //BasicController
+            var control = owner.GetComponent<UnvsPlayer>();
+            if (control!=null)
+            {
+                unvs.editor.utils.Dialogs.Show($"Please, remove {control.GetType()} before add {typeof(BasicController)}");
+                return;
+            }
+            owner.AddComponent<BasicController>();
+        }
+        //[UnvsButton("Create mouse controller")]
+        //public void EditorCreateDefaltMousePlayerController()
+        //{
+        //    //BasicController
+        //    var control = owner.GetComponent<UnvsPlayer>();
+        //    if (control != null)
+        //    {
+        //        unvs.editor.utils.Dialogs.Show($"Please, remove {control.GetType()} before add {typeof(BasicController)}");
+        //        return;
+        //    }
+        //    owner.AddComponent<BasicController>();
+        //}
+    }
+#endif
 }
